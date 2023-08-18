650b95e12f0c9ae68713f71503f738b2e8509dc7
diya1256 <22000028@myrp.edu.sg>
2023-08-18 14:27:53 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22000028/mywebbrowser/MainActivity.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c346.id22000028.mywebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.rp.edu.sg/soi/home";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
